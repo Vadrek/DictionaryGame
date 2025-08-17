d0e3cb56b44b1992039032209782ae9dcac12e3f
Axel Joigneau <axel.joigneau@gmail.com>
2025-08-17 20:32:40 +0200

    fix nextjs build

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'server',
+  output: 'standalone',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
