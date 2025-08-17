f6ec89071a843e96cea87d575f5e8ce6f57d34f0
Axel Joigneau <axel.joigneau@gmail.com>
2025-08-17 22:47:22 +0200

    remove page rooms

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'standalone',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
